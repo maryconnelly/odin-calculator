c8d08ebfc2e127e6bd14d6fb136f79e285ac9aff
Mary Connelly <maryconnelly14@gmail.com>
2024-11-22 16:19:16 -0600

    created initial operator functions and tested

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const operator = "";
+const num1 = "";
+const num2 = "";
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, operator, num2) {
+    add();
+}
+
