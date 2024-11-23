9d8acc4b9bc36589bd34df69aa3721f25de22bec
Mary Connelly <maryconnelly14@gmail.com>
2024-11-22 23:16:12 -0600

    digits working again

--- calculator.js
+++ calculator.js
@@ -19,52 +19,62 @@ function divide(a, b) {
 }
 
 function operate(num1, operator, num2) {
-    add();
+    
 }
 
 const number = "";
 
 const display = document.querySelector("#display");
+    display.textContent = number;
 
 const one = document.querySelector("#one");
     one.addEventListener('click', () => {
-        display.textContent = "1";
+        display.textContent = 1;
     })
+
 const two = document.querySelector("#two");
 two.addEventListener('click', () => {
-    display.textContent = "2";
+    display.textContent = 2;
 })
+
 const three = document.querySelector("#three");
 three.addEventListener('click', () => {
-    display.textContent = "3";
+    display.textContent = 3;
 })
+
 const four = document.querySelector("#four");
 four.addEventListener('click', () => {
-    display.textContent = "4";
+    display.textContent = 4;
 })
+
 const five = document.querySelector("#five");
 five.addEventListener('click', () => {
-    display.textContent = "5";
+    display.textContent = 5;
 })
+
 const six = document.querySelector("#six");
 six.addEventListener('click', () => {
-    display.textContent = "6";
+    display.textContent = 6;
 })
+
 const seven = document.querySelector("#seven");
 seven.addEventListener('click', () => {
-    display.textContent = "7";
+    display.textContent = 7;
 })
+
 const eight = document.querySelector("#eight");
 eight.addEventListener('click', () => {
-    display.textContent = "8";
+    display.textContent = 8;
 })
+
 const nine = document.querySelector("#nine");
 nine.addEventListener('click', () => {
-    display.textContent = "9";
+    display.textContent = 9;
 })
+
 const zero = document.querySelector("#zero");
 zero.addEventListener('click', () => {
-    display.textContent = "0";
+    display.textContent = 0;
 })
 
 const decimal = document.querySelector("#decimal");
