921cce7b6f39184fc70d4878283c0aaa72af8e7e
Mary Connelly <maryconnelly14@gmail.com>
2024-11-22 19:44:52 -0600

    build calculator css and html

--- calculator.js
+++ calculator.js
@@ -22,3 +22,17 @@ function operate(num1, operator, num2) {
     add();
 }
 
+const display = document.querySelector("#display");
+
+const one = document.querySelector("#one");
+ 
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+
