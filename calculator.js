c707adfdf70b1a5cef09f72a4d9865f823f14561
Mary Connelly <maryconnelly14@gmail.com>
2024-11-25 21:17:14 -0600

    Numbers populate display accurately

--- calculator.js
+++ calculator.js
@@ -1,88 +1,62 @@
-const operator = "";
-const num1 = "";
-const num2 = "";
-
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(num1, operator, num2) {
-    
-}
-
-const number = "";
-
 const display = document.querySelector("#display");
-    display.textContent = number;
-
-const one = document.querySelector("#one");
-    one.addEventListener('click', () => {
-        display.textContent = 1;
-    })
-
-const two = document.querySelector("#two");
-two.addEventListener('click', () => {
-    display.textContent = 2;
-})
-
-const three = document.querySelector("#three");
-three.addEventListener('click', () => {
-    display.textContent = 3;
-})
-
-const four = document.querySelector("#four");
-four.addEventListener('click', () => {
-    display.textContent = 4;
-})
-
-const five = document.querySelector("#five");
-five.addEventListener('click', () => {
-    display.textContent = 5;
-})
-
-const six = document.querySelector("#six");
-six.addEventListener('click', () => {
-    display.textContent = 6;
-})
-
-const seven = document.querySelector("#seven");
-seven.addEventListener('click', () => {
-    display.textContent = 7;
-})
-
-const eight = document.querySelector("#eight");
-eight.addEventListener('click', () => {
-    display.textContent = 8;
-})
-
-const nine = document.querySelector("#nine");
-nine.addEventListener('click', () => {
-    display.textContent = 9;
-})
-
-const zero = document.querySelector("#zero");
-zero.addEventListener('click', () => {
-    display.textContent = 0;
-})
-
-const decimal = document.querySelector("#decimal");
-decimal.addEventListener('click', () => {
-    display.textContent = ".";
-})
-
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
+
+let a = 0;
+let b = 0;
+
+let operator = '';
+let reset = false;
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        if (reset || display.textContent == 0) {
+            display.textContent = number.value;
+            reset = false; 
+        } else {
+            display.textContent += number.value;
+            }
+        });
+      });
+
+operators.forEach((operator) => {
+        operator.addEventListener('click', () => {
+            reset = false;
+            a = +display.textContent;
+            operator = operator.value;
+            display.textContent += `${operator}`;
+        })
+});
+
+equals.addEventListener('click', () => {
+    b = +display.textContent.split(' ')[2];
+    operate(a, b, operator);
+});
+
 clear.addEventListener('click', () => {
-    display.textContent = "";
-})
+    (a=0), (b=0), (display.textContent = '');
+    display.textContent = 0;
+});
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            display.textContent = a + b;
+            break;
+        case '-':
+            display.textContent = a - b;
+            break;
+        case '*':
+            display.textContent = a * b;
+            break;
+        case '/':
+            display.textContent = (a / b).toFixed(2);
+            break;
+        default:
+            console.log('error');
+    }
+
+    reset = true;
+}
