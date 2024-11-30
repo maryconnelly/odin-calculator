78e3d79eba04379b50f311230383434fd800fe55
Mary Connelly <maryconnelly14@gmail.com>
2024-11-30 10:46:51 -0600

    Update with backspace button, all works

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ const numbers = document.querySelectorAll(".numberButton");
 const operators = document.querySelectorAll(".operatorButton");
 const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
+const backspace = document.querySelector("#backspace");
 
 let a = 0;
 let b = 0;
@@ -11,14 +12,14 @@ let operator = '';
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-            display.textContent += number.dataset.value;
+            display.textContent += number.value;
             }
     )})
 
-operators.forEach((operator) => {
-        operator.addEventListener('click', () => {
+operators.forEach((localOperator) => {
+    localOperator.addEventListener('click', () => {
             a = +display.textContent;
-            operator = operator.value;
+            operator = localOperator.value;
             display.textContent += ` ${operator} `;
         })
 });
@@ -30,27 +31,32 @@ equals.addEventListener('click', () => {
 
 clear.addEventListener('click', () => {
     a = 0;
-    b = 0;
+    b = 0;  
     operator = "";
     display.textContent = "";
 
 })
 
-let result = 0;
+   backspace.addEventListener('click', () => {
+    display.textContent = display.textContent.substring(0, display.textContent.length-1);
+   })
 
-function operate(a, b, operator) {
-    if (operator = "/") {
-        result = (a / b).toFixed(2);
-            display.textContent = result;
-    } else if (operator = "*") {
-            result = a * b;
-                display.textContent = result;
-        } else if (operator = "-") {
-            result = a - b;
-                display.textContent = result;
-        } else if (operator = "+") {
-                    result = a + b;
-                        display.textContent = result;
-        } else {
+
+
+function operate(a, b, operator) {   
+
+    if (operator === "*") {
+        display.textContent = (a * b).toFixed(2);
+
+    } else if (operator === "/") {
+        display.textContent = (a / b).toFixed(2);
+
+    } else if (operator === "-") {
+        display.textContent = (a - b).toFixed(2);
+
+    } else if (operator === "+") {
+        display.textContent = (a + b).toFixed(2);    
+
+    } else {
           display.textContent = "error"; 
-        }}
+        }};
